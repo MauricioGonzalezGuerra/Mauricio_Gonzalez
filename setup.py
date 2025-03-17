bf84810596cd582adba8b963136a7ec0a6f415cb
MauricioGonzalezGuerra <mauricio.gonzalez@est.iudigital.edu.co>
2025-03-16 21:08:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 setup(
     name="Actividad_2",
     version="0.0.2",
-    author="Danilo Villegas",
-    author_email="danilo.villegas@est.iudigital.edu.co",
+    author="Mauricio Alejandro Gonzalez Guerra",
+    author_email="mauricio.gonzalez@est.iudigital.edu.co",
     description="Evidencia de aprendizaje Numero 2",
-    py_modules=["A2_VILLEGAS_DANILO"],
+    py_modules=["Actividad_2"],
     install_requires=[
         "pandas",
         "matplotlib",
