c8e2532dfabf01273145872fff902eedc8f31a98
MauricioGonzalezGuerra <mauricio.gonzalez@est.iudigital.edu.co>
2025-03-16 21:07:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Actividad_2",
+    version="0.0.2",
+    author="Danilo Villegas",
+    author_email="danilo.villegas@est.iudigital.edu.co",
+    description="Evidencia de aprendizaje Numero 2",
+    py_modules=["A2_VILLEGAS_DANILO"],
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "numpy",
+        "matplotlib",
+        "Scipy"
+    ]
+    
+    
+)
+
