591d55030bbb1fe08365962b32e76bb4627555c1
MauricioGonzalezGuerra <mauricio.gonzalez@est.iudigital.edu.co>
2025-03-30 23:42:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Actividad_2",
+    name="Actividad_3",
     version="0.0.2",
     author="Mauricio Alejandro Gonzalez Guerra",
     author_email="mauricio.gonzalez@est.iudigital.edu.co",
-    description="Evidencia de aprendizaje Numero 2",
-    py_modules=["Actividad_2"],
+    description="Evidencia de aprendizaje Numero 3",
+    py_modules=["Actividad_3"],
     install_requires=[
+         "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
         "pandas",
-        "matplotlib",
         "numpy",
-        "matplotlib",
-        "Scipy"
+        "requests"
     ]
     
     
